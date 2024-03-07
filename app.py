e27e91151c6d3cae69d750204ac991408cc24cde
HAMZA MURTAZA <hamzamurtaza11@yahoo.com>
2024-03-07 21:14:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Assuming the model is relatively large and causing timeouts due to loading times or inference processing,
-# ensure it is loaded efficiently and consider using a lighter model if possible.
 model = load_model("keras_model_hamza_lite.h5", compile=False)
 class_names = [line.strip() for line in open("labels_lite.txt", "r")]
 
@@ -19,10 +17,9 @@ def predict():
         return jsonify({"error": "No file selected"}), 400
 
     try:
-        # Streamlining the image preprocessing
         with Image.open(file).convert("RGB") as image:
-            image = ImageOps.fit(image, (224, 224), Image.ANTIALIAS)
-            image_array = np.asarray(image) / 127.5 - 1  # Combined steps to reduce memory footprint
+            image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
+            image_array = np.asarray(image) / 127.5 - 1  # Normalize the image
 
             prediction = model.predict(np.expand_dims(image_array, axis=0))
             index = np.argmax(prediction)
@@ -32,6 +29,8 @@ def predict():
             })
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
 
 
 
